20b93e4b3d4f7a296c76e7bbb5a93d17e08624e5
Ahmet Çetinkaya <ahmetcetinkaya@tutamail.com>
2024-09-18 09:46:18 +0300

    distinct, group by, having

--- select.sql
+++ select.sql
@@ -24,3 +24,25 @@ WHERE categoryid IN(1,2)
 SELECT * FROM Customers 
 -- WHERE fax IS NULL
 WHERE NOT fax IS NULL
+
+SELECT * FROM 'Order Details' 
+WHERE unitprice * quantity - (unitprice * quantity * discount) > 1000
+
+SELECT COUNT(DISTINCT city) FROM Customers 
+WHERE city IS NOT NULL
+
+SELECT country as 'Ülke', COUNT(DISTINCT city) as 'Benzersiz Şehir' FROM Customers 
+WHERE city IS NOT NULL
+GROUP BY country
+--ORDER BY COUNT(DISTINCT city) ASC
+ORDER BY COUNT(DISTINCT city) DESC
+
+--select DISTINCT contactname || ' - ' || city from Customers
+SELECT DISTINCT CONCAT(contactname, ' - ', city) from Customers
+
+SELECT country as 'Ülke', COUNT(DISTINCT city) as 'Benzersiz Şehir' FROM Customers 
+WHERE city IS NOT NULL
+GROUP BY country
+HAVING COUNT(DISTINCT city) >= 3
+--ORDER BY COUNT(DISTINCT city) ASC
+ORDER BY COUNT(DISTINCT city) DESC
